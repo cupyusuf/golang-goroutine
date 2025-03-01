9bfd99488f49184d9465ffb3b83fc1ade471c132
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-02 06:47:56 +0700

    feat: add buffered channel

--- channel_test.go
+++ channel_test.go
@@ -58,3 +58,22 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 2)
+	defer close(channel)
+
+	go func() {
+		channel <- "Yusuf"
+		channel <- "Supriadi"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Selesai")
+}
