87b47d652b98b3f2226f0a9d571941bef349ff32
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-02 06:25:26 +0700

    feat: add channel

---
+++ channel_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Yusuf Supriadi"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
